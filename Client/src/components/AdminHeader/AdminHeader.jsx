c2aad7be31f45a8ed997008c9f69301c645bcf2f
Henrik Liljestrand <henrik.liljestrand@hotmail.com>
2024-04-17 18:31:32 +0200

    Added logout button on admin header

--- Client/src/components/AdminHeader/AdminHeader.jsx
+++ Client/src/components/AdminHeader/AdminHeader.jsx
@@ -1,12 +1,17 @@
 import './AdminHeader.css'
+import { useNavigate } from 'react-router-dom';
 
 const AdminHeader =()=>{
 
+    const navigate = useNavigate();
+    
     return(<>
         <nav className="navbar bg-dark">
             <div className="container-fluid">
                 <span className="navbar-brand mb-0 h1 text-white"><h2>Aministratör</h2></span>
+                <button onClick={()=>navigate("/")}>Logga ut</button>
             </div>
+            
         </nav>
     </>)
 }
